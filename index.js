ae158e3a196873dd9f5f525b491adeb2dae726fc
unknown <sumail>
2021-11-01 12:45:59 -0300

    responsive

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // REGISTRARSE
-const registrarse = document.getElementById("registrarse");
-const showRegistro = document.getElementById("showRegistro");
+// const registrarse = document.getElementById("registrarse");
+// const showRegistro = document.getElementById("showRegistro");
 
 
 
-showRegistro.onclick = function() {
-    if (registrarse.style.display === "none") {
-        registrarse.style.display = "flex";
-    } else {
-        registrarse.style.display = "none";
-    }
+// showRegistro.onclick = function() {
+//     if (registrarse.style.display === "none") {
+//         registrarse.style.display = "flex";
+//     } else {
+//         registrarse.style.display = "none";
+//     }
 
-};
+// };
 
-// INICIAR SESION
-const inicioSesion = document.getElementById("inicioSesion");
-const showIniciarSesion = document.getElementById("showIniciarSesion");
+// // INICIAR SESION
+// const inicioSesion = document.getElementById("inicioSesion");
+// const showIniciarSesion = document.getElementById("showIniciarSesion");
 
 
 
-showIniciarSesion.onclick = function() {
-    if (inicioSesion.style.display === "none") {
-        inicioSesion.style.display = "block";
-    } else {
-        inicioSesion.style.display = "none";
-    }
+// showIniciarSesion.onclick = function() {
+//     if (inicioSesion.style.display === "none") {
+//         inicioSesion.style.display = "block";
+//     } else {
+//         inicioSesion.style.display = "none";
+//     }
 
-};
+// };
 
 
 // Menu hamburguesa
